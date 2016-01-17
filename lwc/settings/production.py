29834a120bc72a707618231d1dd65289b7c0c466
navicester <navicester@qq.com>
2016-01-18 00:15:05 +0800

    da

--- lwc/settings/production.py
+++ lwc/settings/production.py
@@ -15,10 +15,10 @@ if len(DATABASES['default']) == 0:
 	DATABASES = {
 	    'default': {
 	        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-	        'NAME': 'lwc',                      # Or path to database file if using sqlite3.
+	        'NAME': 'dbv6n78stkmeqg',                      # Or path to database file if using sqlite3.
 	        # The following settings are not used with sqlite3:
-	        'USER': 'root',
-	        'PASSWORD': '123',
+	        'USER': 'ntccscinrvwneo',
+	        'PASSWORD': '5432',
 	        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
 	        'PORT': '',                      # Set to empty string for default.
 	    }
