b7ced5b28c75a8bee3d3ec6964add2a9964f2136
navicester <navicester@qq.com>
2016-01-17 15:02:52 +0800

    requirement

---
+++ lwc/settings/production.py
@@ -0,0 +1,20 @@
+import os
+from django.conf import settings
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+DATABASES = settings.DATABASES
+
+import dj_database_url
+
+# Parse database configuration from $DATABASE_URL
+#DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+SHARE_URL = "http://launchwithcode.com/?ref="
