55b47ec77ef62b89fb65fa89dd02984612f9379a
navicester <navicester@qq.com>
2016-01-17 15:41:47 +0800

    update

--- lwc/settings/base.py
+++ lwc/settings/base.py
@@ -4,6 +4,17 @@ from lwc.settings_old import *
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'lwc',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'root',
+        'PASSWORD': '123',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
 
 '''
 DEBUG = True
