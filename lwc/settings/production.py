83aa58f10f2af8e87b238ea48431b62b3bca5899
navicester <navicester@qq.com>
2016-01-20 21:55:34 +0800

    db

--- lwc/settings/production.py
+++ lwc/settings/production.py
@@ -10,22 +10,31 @@ import dj_database_url
 
 # Parse database configuration from $DATABASE_URL
 DATABASES['default'] =  dj_database_url.config()
+DATABASE_TYPE = 'sqlite3'
 
 print "len(DATABASES['default']) is : %s" % len(DATABASES['default'])
 
 if len(DATABASES['default']) == 0:
-	DATABASES = {
-	    'default': {
-	        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-	        'NAME': 'lwc',                      # Or path to database file if using sqlite3.
-	        # The following settings are not used with sqlite3:
-	        'USER': 'postgres',
-	        'PASSWORD': '123',
-	        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-	        'PORT': '',                      # Set to empty string for default.
-	    }
-	}
-	print "use postgresql_psycopg2 local "
+    if DATABASE_TYPE == 'sqlite3':
+		DATABASES = {
+			'default': {
+				'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+				'NAME': os.path.join(os.path.dirname(os.path.dirname(__file__)),'lwc.db'),                      # Or path to database file if using sqlite3.
+			}
+		}
+    else:
+		DATABASES = {
+			'default': {
+				'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+				'NAME': 'lwc',                      # Or path to database file if using sqlite3.
+				# The following settings are not used with sqlite3:
+				'USER': 'postgres',
+				'PASSWORD': '123',
+				'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+				'PORT': '',                      # Set to empty string for default.
+			}
+		}
+    print "use postgresql_psycopg2 local "
 else:
 	DATABASES = {
 	    'default': {
