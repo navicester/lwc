dbc17b42aa03bb02f9e1033e81e13fa83a737bfa
navicester <navicester@qq.com>
2016-01-20 22:29:39 +0800

    settings

--- lwc/settings/production.py
+++ lwc/settings/production.py
@@ -36,17 +36,25 @@ if len(DATABASES['default']) == 0:
 		}
     print "use postgresql_psycopg2 local "
 else:
-	DATABASES = {
-	    'default': {
-	        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-	        'NAME': 'dbv6n78stkmeqg',                      # Or path to database file if using sqlite3.
-	        # The following settings are not used with sqlite3:
-	        'USER': 'ntccscinrvwneo',
-	        'PASSWORD': ' TJG0ByhrZ5f412ZR6u4AnWUQoy',
-	        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-	        'PORT': '5432',                      # Set to empty string for default.
-	    }
-	}
+    if DATABASE_TYPE == 'sqlite3':
+		DATABASES = {
+			'default': {
+				'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+				'NAME': os.path.join(os.path.dirname(os.path.dirname(__file__)),'lwc.db'),                      # Or path to database file if using sqlite3.
+			}
+		}
+    else:
+		DATABASES = {
+			'default': {
+				'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+				'NAME': 'dbv6n78stkmeqg',                      # Or path to database file if using sqlite3.
+				# The following settings are not used with sqlite3:
+				'USER': 'ntccscinrvwneo',
+				'PASSWORD': ' TJG0ByhrZ5f412ZR6u4AnWUQoy',
+				'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+				'PORT': '5432',                      # Set to empty string for default.
+			}
+		}
 	print "use postgresql_psycopg2 server"
 
 
