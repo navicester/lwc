e228e8b07fe9d18cc0e6676d952b660368fdd38c
navicester <navicester@qq.com>
2016-02-11 01:56:06 +0800

    add all

--- lwc/apache/wsgi.py
+++ lwc/apache/wsgi.py
@@ -30,10 +30,3 @@ application = get_wsgi_application()
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
 # application = HelloWorldApplication(application)
-
-try:
-	from dj_static import Cling
-
-	application = Cling(get_wsgi_application())
-except:
-	pass
