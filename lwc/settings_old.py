b7d737c75dca9bad1cc66f8f9c988ee11e885e68
navicester <navicester@qq.com>
2016-02-11 22:33:24 +0800

    l

--- lwc/settings_old.py
+++ lwc/settings_old.py
@@ -70,7 +70,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static','media')
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = '/media_lwc/'
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -80,7 +80,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static','static_root')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static_lwc/'
+STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
