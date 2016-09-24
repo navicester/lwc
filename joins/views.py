13660d3a71dd99d60954b0bb8d67b49ed4ad9f9a
navicester <navicester@qq.com>
2016-09-24 23:27:37 +0800

    align with server

--- joins/views.py
+++ joins/views.py
@@ -7,7 +7,7 @@ from .models import Join
 
 def get_ip(request):
 	try:
-		x_forward = request.META.get("HTTP_X_FORWORDED_FOR")
+		x_forward = request.META.get("HTTP_X_FORWARDED_FOR")
 		if x_forward:
 			ip = x_forward.split(',')[0]
 		else:
@@ -25,36 +25,7 @@ def get_ref_id():
 	except:
 		return ref_id
 
-def home(request):
-	try:
-		join_id = request.session['join_id_ref']
-		obj = Join.objects.get(id=join_id)
-	except:
-		obj = None	
-	'''
-	form = EmailForm(request.POST or None)
-	if form.is_valid():
-		email =  form.cleaned_data['email']
-		new_join, created = Joins.objects.get_or_create(email=email)
-		print new_join, created
-	'''
 
-	form = JoinForm(request.POST or None)
-	if form.is_valid():
-		new_join = form.save(commit = False)
-		email = form.cleaned_data['email']
-		new_join_old, created = Join.objects.get_or_create(email = email)
-		if created:
-			new_join_old.ref_id = get_ref_id()
-			if not obj == None:
-				new_join_old.friend = obj
-			new_join_old.ip_address = get_ip(request)
-			new_join_old.save()
-		return HttpResponseRedirect("/%s" %(new_join_old.ref_id))
-		
-	context = {"form":form}
-	template = "home.html"
-	return render(request, template, context)
 
 def share(request, ref_id):
 
@@ -94,3 +65,33 @@ def share(request, ref_id):
         return render(request, template, context)
     except:
         raise Http404
+def home(request):
+	try:
+		join_id = request.session['join_id_ref']
+		obj = Join.objects.get(id=join_id)
+	except:
+		obj = None	
+	'''
+	form = EmailForm(request.POST or None)
+	if form.is_valid():
+		email =  form.cleaned_data['email']
+		new_join, created = Joins.objects.get_or_create(email=email)
+		print new_join, created
+	'''
+
+	form = JoinForm(request.POST or None)
+	if form.is_valid():
+		new_join = form.save(commit = False)
+		email = form.cleaned_data['email']
+		new_join_old, created = Join.objects.get_or_create(email = email)
+		if created:
+			new_join_old.ref_id = get_ref_id()
+			if not obj == None:
+				new_join_old.friend = obj
+			new_join_old.ip_address = get_ip(request)
+			new_join_old.save()
+		return HttpResponseRedirect("/%s" %(new_join_old.ref_id))
+		
+	context = {"form":form}
+	template = "home.html"
+	return render(request, template, context)
