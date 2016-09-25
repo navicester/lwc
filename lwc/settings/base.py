9f99f5fe23f7a3fac8c450b74e3e7e60d3aa286e
navicester <navicester@qq.com>
2016-09-25 09:14:42 +0800

    x

--- lwc/settings/base.py
+++ lwc/settings/base.py
@@ -19,7 +19,7 @@ TEMPLATE_DEBUG = True
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 # print TEMPLATE_DIRS
 INSTALLED_APPS = (    
